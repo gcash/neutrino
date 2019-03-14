4b8100c633d81f5aaac0310b0878322838ece0f4
Johan T. Halseth <johanth@gmail.com>
2019-03-14 13:20:16 +0100

    query+cache: move FilterCacheKey to cache package

--- cache/cacheable_filter.go
+++ cache/cacheable_filter.go
@@ -1,6 +1,16 @@
 package cache
 
-import "github.com/gcash/bchutil/gcs"
+import (
+	"github.com/gcash/bchd/chaincfg/chainhash"
+	"github.com/gcash/bchutil/gcs"
+	"github.com/gcash/neutrino/filterdb"
+)
+
+// filterCacheKey represents the key used to access filters in the FilterCache.
+type FilterCacheKey struct {
+	BlockHash  *chainhash.Hash
+	FilterType filterdb.FilterType
+}
 
 // CacheableFilter is a wrapper around Filter type which provides a Size method
 // used by the cache to target certain memory usage.
