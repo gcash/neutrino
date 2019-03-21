treyward32/pearl

multi: replace block notifications with new subscription manager
In this commit, we replace the current block notification system with
the one provided by the recently introduced
blockntfns.SubscriptionManager. Much of the logic is still the same,
this is merely a refactor to obtain a cleaner abstraction. Notifications
are streamed through a single channel, rather than two, in order to
ensure they are processed in their correct order.

spv/blockmanager.go
@@ -20,6 +20,7 @@ import (
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcutil/gcs"
 	"github.com/btcsuite/btcutil/gcs/builder"
+	"github.com/lightninglabs/neutrino/blockntfns"
 	"github.com/lightninglabs/neutrino/headerfs"
 	"github.com/lightninglabs/neutrino/headerlist"
 )
@@ -167,6 +168,10 @@ type blockManager struct {
 	// try to perform any queries before we have our first peer.
 	firstPeerSignal <-chan struct{}
 
+	// blockNtfnChan is a channel in which the latest block notifications
+	// for the tip of the chain will be sent upon.
+	blockNtfnChan chan blockntfns.BlockNtfn
+
 	wg   sync.WaitGroup
 	quit chan struct{}
 
@@ -191,8 +196,9 @@ func newBlockManager(s *ChainService,
 	adjustmentFactor := s.chainParams.RetargetAdjustmentFactor
 
 	bm := blockManager{
-		server:   s,
-		peerChan: make(chan interface{}, MaxPeers*3),
+		server:        s,
+		peerChan:      make(chan interface{}, MaxPeers*3),
+		blockNtfnChan: make(chan blockntfns.BlockNtfn),
 		blkHeaderProgressLogger: newBlockProgressLogger(
 			"Processed", "block", log,
 		),
@@ -1110,7 +1116,6 @@ func (b *blockManager) writeCFHeadersMsg(msg *wire.MsgCFHeaders,
 
 	// Notify subscribers, and also update the filter header progress
 	// logger at the same time.
-	msgType := connectBasic
 	for i, header := range matchingBlockHeaders {
 		header := header
 
@@ -1119,10 +1124,7 @@ func (b *blockManager) writeCFHeadersMsg(msg *wire.MsgCFHeaders,
 			header.Timestamp, int32(headerHeight),
 		)
 
-		b.server.sendSubscribedMsg(&blockMessage{
-			msgType: msgType,
-			header:  &header,
-		})
+		b.onBlockConnected(header, headerHeight)
 	}
 
 	// We'll also set the new header tip and notify any peers that the tip
@@ -2545,4 +2547,77 @@ func (b *blockManager) findPrevTestNetDifficulty(hList headerlist.Chain) (uint32
 		lastBits = iterNode.Bits
 	}
 	return lastBits, nil
+}
+
+// onBlockConnected queues a block notification that extends the current chain.
+func (b *blockManager) onBlockConnected(header wire.BlockHeader, height uint32) {
+	select {
+	case b.blockNtfnChan <- blockntfns.NewBlockConnected(header, height):
+	case <-b.quit:
+	}
+}
+
+// onBlockDisconnected queues a block notification that reorgs the current
+// chain.
+func (b *blockManager) onBlockDisconnected(headerDisconnected wire.BlockHeader,
+	heightDisconnected uint32, newChainTip wire.BlockHeader) {
+
+	select {
+	case b.blockNtfnChan <- blockntfns.NewBlockDisconnected(
+		headerDisconnected, heightDisconnected, newChainTip,
+	):
+	case <-b.quit:
+	}
+}
+
+// Notifications exposes a receive-only channel in which the latest block
+// notifications for the tip of the chain can be received.
+func (b *blockManager) Notifications() <-chan blockntfns.BlockNtfn {
+	return b.blockNtfnChan
+}
+
+// NotificationsSinceHeight returns a backlog of block notifications starting
+// from the given height to the tip of the chain. When providing a height of 0,
+// a backlog will not be delivered.
+func (b *blockManager) NotificationsSinceHeight(
+	height uint32) ([]blockntfns.BlockNtfn, uint32, error) {
+
+	b.newFilterHeadersMtx.RLock()
+	defer b.newFilterHeadersMtx.RUnlock()
+
+	bestHeight := b.filterHeaderTip
+
+	// If a height of 0 is provided by the caller, then a backlog of
+	// notifications is not needed.
+	if height == 0 {
+		return nil, bestHeight, nil
+	}
+
+	// If the best height matches the filter header tip, then we're done and
+	// don't need to proceed any further.
+	if bestHeight == height {
+		return nil, bestHeight, nil
+	}
+
+	// If the request has a height later than a height we've yet to come
+	// across in the chain, we'll return an error to indicate so to the
+	// caller.
+	if height > bestHeight {
+		return nil, 0, fmt.Errorf("request with height %d is greater "+
+			"than best height known %d", height, bestHeight)
+	}
+
+	// Otherwise, we need to read block headers from disk to deliver a
+	// backlog to the caller before we proceed.
+	blocks := make([]blockntfns.BlockNtfn, 0, bestHeight-height)
+	for i := height + 1; i <= bestHeight; i++ {
+		header, err := b.server.BlockHeaders.FetchHeaderByHeight(i)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		blocks = append(blocks, blockntfns.NewBlockConnected(*header, i))
+	}
+
+	return blocks, bestHeight, nil
 }

spv/blockmanager_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil/gcs/builder"
 	"github.com/btcsuite/btcwallet/walletdb"
+	"github.com/lightninglabs/neutrino/blockntfns"
 	"github.com/lightninglabs/neutrino/headerfs"
 )
 
@@ -376,6 +377,22 @@ func TestBlockManagerInitialInterval(t *testing.T) {
 			}
 		}
 
+		// We should expect to see notifications for each new filter
+		// header being connected.
+		startHeight := uint32(1)
+		if test.partialInterval {
+			startHeight = wire.CFCheckptInterval / 3
+		}
+		go func() {
+			for i := startHeight; i <= maxHeight; i++ {
+				ntfn := <-bm.blockNtfnChan
+				if _, ok := ntfn.(*blockntfns.Connected); !ok {
+					t.Fatal("expected block connected " +
+						"notification")
+				}
+			}
+		}()
+
 		// Call the get checkpointed cf headers method with the
 		// checkpoints we created to start the test.
 		bm.getCheckpointedCFHeaders(
@@ -583,6 +600,22 @@ func TestBlockManagerInvalidInterval(t *testing.T) {
 				}
 			}
 		}
+
+		// We should expect to see notifications for each new filter
+		// header being connected.
+		startHeight := uint32(1)
+		if test.partialInterval {
+			startHeight = wire.CFCheckptInterval / 3
+		}
+		go func() {
+			for i := startHeight; i <= maxHeight; i++ {
+				ntfn := <-bm.blockNtfnChan
+				if _, ok := ntfn.(*blockntfns.Connected); !ok {
+					t.Fatal("expected block connected " +
+						"notification")
+				}
+			}
+		}()
 
 		// Start the test by calling the get checkpointed cf headers
 		// method with the checkpoints we created.

spv/neutrino.go
@@ -22,6 +22,7 @@ import (
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcwallet/waddrmgr"
 	"github.com/btcsuite/btcwallet/walletdb"
+	"github.com/lightninglabs/neutrino/blockntfns"
 	"github.com/lightninglabs/neutrino/cache/lru"
 	"github.com/lightninglabs/neutrino/filterdb"
 	"github.com/lightninglabs/neutrino/headerfs"
@@ -522,34 +523,25 @@ type ChainService struct {
 	queryBatch func([]wire.Message, func(*ServerPeer, wire.Message,
 		wire.Message) bool, <-chan struct{}, ...QueryOption)
 
-	chainParams       chaincfg.Params
-	addrManager       *addrmgr.AddrManager
-	connManager       *connmgr.ConnManager
-	blockManager      *blockManager
-	newPeers          chan *ServerPeer
-	donePeers         chan *ServerPeer
-	banPeers          chan *ServerPeer
-	query             chan interface{}
-	firstPeerConnect  chan struct{}
-	peerHeightsUpdate chan updatePeerHeightsMsg
-	wg                sync.WaitGroup
-	quit              chan struct{}
-	timeSource        blockchain.MedianTimeSource
-	services          wire.ServiceFlag
-	blockSubscribers  map[*blockSubscription]struct{}
-	mtxSubscribers    sync.RWMutex
-	utxoScanner       *UtxoScanner
+	chainParams          chaincfg.Params
+	addrManager          *addrmgr.AddrManager
+	connManager          *connmgr.ConnManager
+	blockManager         *blockManager
+	blockSubscriptionMgr *blockntfns.SubscriptionManager
+	newPeers             chan *ServerPeer
+	donePeers            chan *ServerPeer
+	banPeers             chan *ServerPeer
+	query                chan interface{}
+	firstPeerConnect     chan struct{}
+	peerHeightsUpdate    chan updatePeerHeightsMsg
+	wg                   sync.WaitGroup
+	quit                 chan struct{}
+	timeSource           blockchain.MedianTimeSource
+	services             wire.ServiceFlag
+	utxoScanner          *UtxoScanner
 
 	// TODO: Add a map for more granular exclusion?
 	mtxCFilter sync.Mutex
-
-	// These are only necessary until the block subscription logic is
-	// refactored out into its own package and we can have different message
-	// types sent in the notifications.
-	//
-	// TODO(aakselrod): Get rid of this when doing the refactoring above.
-	reorgedBlockHeaders map[chainhash.Hash]*wire.BlockHeader
-	mtxReorgHeader      sync.RWMutex
 
 	userAgentName    string
 	userAgentVersion string
@@ -594,23 +586,21 @@ func NewChainService(cfg Config) (*ChainService, error) {
 	amgr := addrmgr.New(cfg.DataDir, nameResolver)
 
 	s := ChainService{
-		chainParams:         cfg.ChainParams,
-		addrManager:         amgr,
-		newPeers:            make(chan *ServerPeer, MaxPeers),
-		donePeers:           make(chan *ServerPeer, MaxPeers),
-		banPeers:            make(chan *ServerPeer, MaxPeers),
-		query:               make(chan interface{}),
-		quit:                make(chan struct{}),
-		firstPeerConnect:    make(chan struct{}),
-		peerHeightsUpdate:   make(chan updatePeerHeightsMsg),
-		timeSource:          blockchain.NewMedianTime(),
-		services:            Services,
-		userAgentName:       UserAgentName,
-		userAgentVersion:    UserAgentVersion,
-		blockSubscribers:    make(map[*blockSubscription]struct{}),
-		reorgedBlockHeaders: make(map[chainhash.Hash]*wire.BlockHeader),
-		nameResolver:        nameResolver,
-		dialer:              dialer,
+		chainParams:       cfg.ChainParams,
+		addrManager:       amgr,
+		newPeers:          make(chan *ServerPeer, MaxPeers),
+		donePeers:         make(chan *ServerPeer, MaxPeers),
+		banPeers:          make(chan *ServerPeer, MaxPeers),
+		query:             make(chan interface{}),
+		quit:              make(chan struct{}),
+		firstPeerConnect:  make(chan struct{}),
+		peerHeightsUpdate: make(chan updatePeerHeightsMsg),
+		timeSource:        blockchain.NewMedianTime(),
+		services:          Services,
+		userAgentName:     UserAgentName,
+		userAgentVersion:  UserAgentVersion,
+		nameResolver:      nameResolver,
+		dialer:            dialer,
 	}
 
 	// We set the queryPeers method to point to queryChainServicePeers,
@@ -664,6 +654,7 @@ func NewChainService(cfg Config) (*ChainService, error) {
 		return nil, err
 	}
 	s.blockManager = bm
+	s.blockSubscriptionMgr = blockntfns.NewSubscriptionManager(s.blockManager)
 
 	// Only setup a function to return new addresses to connect to when not
 	// running in connect-only mode.  The simulation network is always in
@@ -896,22 +887,15 @@ func (s *ChainService) rollBackToHeight(height uint32) (*waddrmgr.BlockStamp, er
 		// header in the disconnected notification in case we're rolling
 		// back farther and the notification subscriber needs it but
 		// can't read it before it's deleted from the store.
-		//
-		// TODO(aakselrod): Get rid of this when subscriptions are
-		// factored out into their own package.
-		lastHeader, _, err := s.BlockHeaders.FetchHeader(newTip)
+		prevHeader, _, err := s.BlockHeaders.FetchHeader(newTip)
 		if err != nil {
 			return nil, err
 		}
-		s.mtxReorgHeader.Lock()
-		s.reorgedBlockHeaders[header.PrevBlock] = lastHeader
-		s.mtxReorgHeader.Unlock()
 
 		// Now we send the block disconnected notifications.
-		s.sendSubscribedMsg(&blockMessage{
-			msgType: disconnect,
-			header:  header,
-		})
+		s.blockManager.onBlockDisconnected(
+			*header, headerHeight, *prevHeader,
+		)
 	}
 	return bs, nil
 }
@@ -927,6 +911,7 @@ func (s *ChainService) peerHandler() {
 	// and stop them in this handler.
 	s.addrManager.Start()
 	s.blockManager.Start()
+	s.blockSubscriptionMgr.Start()
 	s.utxoScanner.Start()
 
 	state := &peerState{
@@ -985,6 +970,7 @@ out:
 
 	s.connManager.Stop()
 	s.utxoScanner.Stop()
+	s.blockSubscriptionMgr.Stop()
 	s.blockManager.Stop()
 	s.addrManager.Stop()
 

gcash/neutrino

blockmanager.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"container/list"
 	"fmt"
+	"github.com/gcash/neutrino/blockntfns"
 	"github.com/go-errors/errors"
 	"math"
 	"math/big"
@@ -168,6 +169,10 @@ type blockManager struct {
 	// try to perform any queries before we have our first peer.
 	firstPeerSignal <-chan struct{}
 
+	// blockNtfnChan is a channel in which the latest block notifications
+	// for the tip of the chain will be sent upon.
+	blockNtfnChan chan blockntfns.BlockNtfn
+
 	wg   sync.WaitGroup
 	quit chan struct{}
 
@@ -194,8 +199,9 @@ func newBlockManager(s *ChainService,
 	adjustmentFactor := s.chainParams.RetargetAdjustmentFactor
 
 	bm := blockManager{
-		server:   s,
-		peerChan: make(chan interface{}, MaxPeers*3),
+		server:        s,
+		peerChan:      make(chan interface{}, MaxPeers*3),
+		blockNtfnChan: make(chan blockntfns.BlockNtfn),
 		blkHeaderProgressLogger: newBlockProgressLogger(
 			"Processed", "block", log,
 		),
@@ -1117,7 +1123,6 @@ func (b *blockManager) writeCFHeadersMsg(msg *wire.MsgCFHeaders,
 
 	// Notify subscribers, and also update the filter header progress
 	// logger at the same time.
-	msgType := connectBasic
 	for i, header := range matchingBlockHeaders {
 		header := header
 
@@ -1126,10 +1131,7 @@ func (b *blockManager) writeCFHeadersMsg(msg *wire.MsgCFHeaders,
 			header.Timestamp, int32(headerHeight),
 		)
 
-		b.server.sendSubscribedMsg(&blockMessage{
-			msgType: msgType,
-			header:  &header,
-		})
+		b.onBlockConnected(header, headerHeight)
 	}
 
 	// We'll also set the new header tip and notify any peers that the tip
@@ -2801,4 +2803,77 @@ func (b *blockManager) findPrevTestNetDifficulty(hList headerlist.Chain) (uint32
 		lastBits = iterNode.Bits
 	}
 	return lastBits, nil
+}
+
+// onBlockConnected queues a block notification that extends the current chain.
+func (b *blockManager) onBlockConnected(header wire.BlockHeader, height uint32) {
+	select {
+	case b.blockNtfnChan <- blockntfns.NewBlockConnected(header, height):
+	case <-b.quit:
+	}
+}
+
+// onBlockDisconnected queues a block notification that reorgs the current
+// chain.
+func (b *blockManager) onBlockDisconnected(headerDisconnected wire.BlockHeader,
+	heightDisconnected uint32, newChainTip wire.BlockHeader) {
+
+	select {
+	case b.blockNtfnChan <- blockntfns.NewBlockDisconnected(
+		headerDisconnected, heightDisconnected, newChainTip,
+	):
+	case <-b.quit:
+	}
+}
+
+// Notifications exposes a receive-only channel in which the latest block
+// notifications for the tip of the chain can be received.
+func (b *blockManager) Notifications() <-chan blockntfns.BlockNtfn {
+	return b.blockNtfnChan
+}
+
+// NotificationsSinceHeight returns a backlog of block notifications starting
+// from the given height to the tip of the chain. When providing a height of 0,
+// a backlog will not be delivered.
+func (b *blockManager) NotificationsSinceHeight(
+	height uint32) ([]blockntfns.BlockNtfn, uint32, error) {
+
+	b.newFilterHeadersMtx.RLock()
+	defer b.newFilterHeadersMtx.RUnlock()
+
+	bestHeight := b.filterHeaderTip
+
+	// If a height of 0 is provided by the caller, then a backlog of
+	// notifications is not needed.
+	if height == 0 {
+		return nil, bestHeight, nil
+	}
+
+	// If the best height matches the filter header tip, then we're done and
+	// don't need to proceed any further.
+	if bestHeight == height {
+		return nil, bestHeight, nil
+	}
+
+	// If the request has a height later than a height we've yet to come
+	// across in the chain, we'll return an error to indicate so to the
+	// caller.
+	if height > bestHeight {
+		return nil, 0, fmt.Errorf("request with height %d is greater "+
+			"than best height known %d", height, bestHeight)
+	}
+
+	// Otherwise, we need to read block headers from disk to deliver a
+	// backlog to the caller before we proceed.
+	blocks := make([]blockntfns.BlockNtfn, 0, bestHeight-height)
+	for i := height + 1; i <= bestHeight; i++ {
+		header, err := b.server.BlockHeaders.FetchHeaderByHeight(i)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		blocks = append(blocks, blockntfns.NewBlockConnected(*header, i))
+	}
+
+	return blocks, bestHeight, nil
 }

blockmanager_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/gcash/bchd/wire"
 	"github.com/gcash/bchutil/gcs/builder"
 	"github.com/gcash/bchwallet/walletdb"
+	"github.com/gcash/neutrino/blockntfns"
 	"github.com/gcash/neutrino/headerfs"
 )
 
@@ -376,6 +377,22 @@ func TestBlockManagerInitialInterval(t *testing.T) {
 			}
 		}
 
+		// We should expect to see notifications for each new filter
+		// header being connected.
+		startHeight := uint32(1)
+		if test.partialInterval {
+			startHeight = wire.CFCheckptInterval / 3
+		}
+		go func() {
+			for i := startHeight; i <= maxHeight; i++ {
+				ntfn := <-bm.blockNtfnChan
+				if _, ok := ntfn.(*blockntfns.Connected); !ok {
+					t.Fatal("expected block connected " +
+						"notification")
+				}
+			}
+		}()
+
 		// Call the get checkpointed cf headers method with the
 		// checkpoints we created to start the test.
 		bm.getCheckpointedCFHeaders(
@@ -583,6 +600,22 @@ func TestBlockManagerInvalidInterval(t *testing.T) {
 				}
 			}
 		}
+
+		// We should expect to see notifications for each new filter
+		// header being connected.
+		startHeight := uint32(1)
+		if test.partialInterval {
+			startHeight = wire.CFCheckptInterval / 3
+		}
+		go func() {
+			for i := startHeight; i <= maxHeight; i++ {
+				ntfn := <-bm.blockNtfnChan
+				if _, ok := ntfn.(*blockntfns.Connected); !ok {
+					t.Fatal("expected block connected " +
+						"notification")
+				}
+			}
+		}()
 
 		// Start the test by calling the get checkpointed cf headers
 		// method with the checkpoints we created.

neutrino.go
@@ -23,6 +23,7 @@ import (
 	"github.com/gcash/bchutil"
 	"github.com/gcash/bchwallet/waddrmgr"
 	"github.com/gcash/bchwallet/walletdb"
+	"github.com/gcash/neutrino/blockntfns"
 	"github.com/gcash/neutrino/cache/lru"
 	"github.com/gcash/neutrino/filterdb"
 	"github.com/gcash/neutrino/headerfs"
@@ -546,34 +547,25 @@ type ChainService struct {
 	queryBatch func([]wire.Message, func(*ServerPeer, wire.Message,
 		wire.Message) bool, <-chan struct{}, ...QueryOption)
 
-	chainParams       chaincfg.Params
-	addrManager       *addrmgr.AddrManager
-	connManager       *connmgr.ConnManager
-	blockManager      *blockManager
-	newPeers          chan *ServerPeer
-	donePeers         chan *ServerPeer
-	banPeers          chan *ServerPeer
-	query             chan interface{}
-	firstPeerConnect  chan struct{}
-	peerHeightsUpdate chan updatePeerHeightsMsg
-	wg                sync.WaitGroup
-	quit              chan struct{}
-	timeSource        blockchain.MedianTimeSource
-	services          wire.ServiceFlag
-	blockSubscribers  map[*blockSubscription]struct{}
-	mtxSubscribers    sync.RWMutex
-	utxoScanner       *UtxoScanner
+	chainParams          chaincfg.Params
+	addrManager          *addrmgr.AddrManager
+	connManager          *connmgr.ConnManager
+	blockManager         *blockManager
+	blockSubscriptionMgr *blockntfns.SubscriptionManager
+	newPeers             chan *ServerPeer
+	donePeers            chan *ServerPeer
+	banPeers             chan *ServerPeer
+	query                chan interface{}
+	firstPeerConnect     chan struct{}
+	peerHeightsUpdate    chan updatePeerHeightsMsg
+	wg                   sync.WaitGroup
+	quit                 chan struct{}
+	timeSource           blockchain.MedianTimeSource
+	services             wire.ServiceFlag
+	utxoScanner          *UtxoScanner
 
 	// TODO: Add a map for more granular exclusion?
 	mtxCFilter sync.Mutex
-
-	// These are only necessary until the block subscription logic is
-	// refactored out into its own package and we can have different message
-	// types sent in the notifications.
-	//
-	// TODO(aakselrod): Get rid of this when doing the refactoring above.
-	reorgedBlockHeaders map[chainhash.Hash]*wire.BlockHeader
-	mtxReorgHeader      sync.RWMutex
 
 	userAgentName    string
 	userAgentVersion string
@@ -624,26 +616,24 @@ func NewChainService(cfg Config) (*ChainService, error) {
 	amgr := addrmgr.New(cfg.DataDir, nameResolver)
 
 	s := ChainService{
-		chainParams:         cfg.ChainParams,
-		addrManager:         amgr,
-		newPeers:            make(chan *ServerPeer, MaxPeers),
-		donePeers:           make(chan *ServerPeer, MaxPeers),
-		banPeers:            make(chan *ServerPeer, MaxPeers),
-		query:               make(chan interface{}),
-		quit:                make(chan struct{}),
-		firstPeerConnect:    make(chan struct{}),
-		peerHeightsUpdate:   make(chan updatePeerHeightsMsg),
-		timeSource:          blockchain.NewMedianTime(),
-		services:            Services,
-		userAgentName:       UserAgentName,
-		userAgentVersion:    UserAgentVersion,
-		blockSubscribers:    make(map[*blockSubscription]struct{}),
-		reorgedBlockHeaders: make(map[chainhash.Hash]*wire.BlockHeader),
-		nameResolver:        nameResolver,
-		dialer:              dialer,
-		blocksOnly:          cfg.BlocksOnly,
-		mempool:             NewMempool(),
-		proxy:               cfg.Proxy,
+		chainParams:       cfg.ChainParams,
+		addrManager:       amgr,
+		newPeers:          make(chan *ServerPeer, MaxPeers),
+		donePeers:         make(chan *ServerPeer, MaxPeers),
+		banPeers:          make(chan *ServerPeer, MaxPeers),
+		query:             make(chan interface{}),
+		quit:              make(chan struct{}),
+		firstPeerConnect:  make(chan struct{}),
+		peerHeightsUpdate: make(chan updatePeerHeightsMsg),
+		timeSource:        blockchain.NewMedianTime(),
+		services:          Services,
+		userAgentName:     UserAgentName,
+		userAgentVersion:  UserAgentVersion,
+		nameResolver:      nameResolver,
+		dialer:            dialer,
+		blocksOnly:        cfg.BlocksOnly,
+		mempool:           NewMempool(),
+		proxy:             cfg.Proxy,
 	}
 
 	// We set the queryPeers method to point to queryChainServicePeers,
@@ -697,6 +687,7 @@ func NewChainService(cfg Config) (*ChainService, error) {
 		return nil, err
 	}
 	s.blockManager = bm
+	s.blockSubscriptionMgr = blockntfns.NewSubscriptionManager(s.blockManager)
 
 	// Only setup a function to return new addresses to connect to when not
 	// running in connect-only mode.  The simulation network is always in
@@ -941,22 +932,15 @@ func (s *ChainService) rollBackToHeight(height uint32) (*waddrmgr.BlockStamp, er
 		// header in the disconnected notification in case we're rolling
 		// back farther and the notification subscriber needs it but
 		// can't read it before it's deleted from the store.
-		//
-		// TODO(aakselrod): Get rid of this when subscriptions are
-		// factored out into their own package.
-		lastHeader, _, err := s.BlockHeaders.FetchHeader(newTip)
+		prevHeader, _, err := s.BlockHeaders.FetchHeader(newTip)
 		if err != nil {
 			return nil, err
 		}
-		s.mtxReorgHeader.Lock()
-		s.reorgedBlockHeaders[header.PrevBlock] = lastHeader
-		s.mtxReorgHeader.Unlock()
 
 		// Now we send the block disconnected notifications.
-		s.sendSubscribedMsg(&blockMessage{
-			msgType: disconnect,
-			header:  header,
-		})
+		s.blockManager.onBlockDisconnected(
+			*header, headerHeight, *prevHeader,
+		)
 	}
 	return bs, nil
 }
@@ -972,6 +956,7 @@ func (s *ChainService) peerHandler() {
 	// and stop them in this handler.
 	s.addrManager.Start()
 	s.blockManager.Start()
+	s.blockSubscriptionMgr.Start()
 	s.utxoScanner.Start()
 
 	state := &peerState{
@@ -1030,6 +1015,7 @@ out:
 
 	s.connManager.Stop()
 	s.utxoScanner.Stop()
+	s.blockSubscriptionMgr.Stop()
 	s.blockManager.Stop()
 	s.addrManager.Stop()
 